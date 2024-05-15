c3740aefc43a0bd54d58decf74a1ea617604b8a9
Villeman <villeman.arm@gmail.com>
2024-05-15 16:54:19 +0300

    feat: getting data from server

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import { reactive, ref, computed, onMounted, onUpdated, watch } from 'vue'
-import NewComponent from './components/NewComponent.vue'
+import axios from 'axios'
+
+import CharacterCard from './components/CharacterCard.vue'
+
+const serverURL: string = 'https://rickandmortyapi.com/graphql'
+
+const charactersList = ref([])
+const currentPage = ref<number>(1)
+
+const getData = (query: string) => {
+  axios.post(serverURL, {query})
+    .then(function (response) {
+      charactersList.value = JSON.parse(response.request.response).data.characters.results
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+onMounted(() => {
+  const starterQuery = `
+      query {
+        characters (page: ${currentPage.value}) {
+          results {
+            id
+            image
+            name
+            status
+            type
+            location {
+              name
+            }
+            episode {
+              name
+            }
+          }
+        }
+      }
+    `
+  getData(starterQuery)
+})
+
 </script>
 
-
-
 <template>
-  <NewComponent msg="You did it!" />
+
+  <!-- <CharacterCard class="" v-for="character in charactersList" :key="character.id" :character="character"/> -->
 
 </template>
 
 <style scoped lang="sass">
   @import '@/assets/styles/constants.sass'
 
-</style>
+</style>./components/CharacterCard.vue
---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import { reactive, ref, computed, onMounted, onUpdated, watch } from 'vue'
+
+defineProps<{
+	character: object;
+}>();
+
+</script>
+
+<template>
+
+	<article class="characterCard__Wrapper-sc-1ejywvi-0 bEklxv">
+		<div class="characterCard__ImgWrapper-sc-1ejywvi-1 fkUcVI">
+			<img src="https://rickandmortyapi.com/api/character/avatar/75.jpeg" alt="Courier Flap">
+		</div>
+		<div class="characterCard__ContentWrapper-sc-1ejywvi-2 isMAic">
+			<div class="section">
+				<a href="https://rickandmortyapi.com/api/character/75" rel="noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">
+					<h2>Courier Flap</h2>
+				</a>
+				<span class="status">
+				<span class="status__icon">
+				</span> Alive - Alien</span>
+			</div>
+			<div class="section">
+				<span class="text-gray">Last known location:</span>
+				<a href="https://rickandmortyapi.com/api/location/35" rel="noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">Planet Squanch</a>
+			</div>
+			<div class="section">
+				<span class="text-gray">First seen in:</span>
+				<a href="https://rickandmortyapi.com/api/episode/21" rel="noopener noreferrer" target="_blank" class="externalLink__ExternalLink-sc-1lixk38-0 ffGNdR">The Wedding Squanchers</a>
+			</div>
+		</div>
+	</article>
+
+</template>
+
+<style scoped lang="sass">
+	@import '@/assets/styles/constants.sass'
+
+
+    
+</style>
+
+
+	// -webkit-text-size-adjust: 100%;
+    // font: 112.5%/1.625 -apple-system,'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial',//sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';
+    // color: hsla(0,0%,0%,0.8);
+    // font-family: -apple-system,'BlinkMacSystemFont','Segoe UI','Roboto','Helvetica','Arial',sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';
+    // font-weight: normal;
+    // word-wrap: break-word;
+    // font-kerning: normal;
+    // font-feature-settings: "kern", "liga", "clig", "calt";
+    // font-variant-ligatures: none;
+    // text-rendering: optimizelegibility;
+    // -webkit-font-smoothing: antialiased;
+    // text-decoration-skip-ink: auto;
+    // box-sizing: inherit;
+    // width: 600px;
+    // height: 220px;
+
+
